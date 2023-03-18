27d5c71f7969d0059bedfd2f2c63e00985cdf4e0
SangJoon Lee <qkqh34k@gmail.com>
2023-03-18 21:41:42 +0900

    log4j2

--- src/main/java/com/ssang/gtd/member/Member.java
+++
@@ -1,4 +0,0 @@
-package com.ssang.gtd.member;
-
-public class Member {
-}
--- src/main/java/com/ssang/gtd/things/GettingController.java
+++ src/main/java/com/ssang/gtd/things/GettingController.java
@@ -1,12 +1,14 @@
 package com.ssang.gtd.things;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GettingController {
-
+    private static final Logger logger = LogManager.getLogger(GettingController.class);
     private final  GettingService gettingService;
 
     public GettingController(GettingService gettingService) {
