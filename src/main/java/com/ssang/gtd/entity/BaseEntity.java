88716c117d0fbbc7c70b50eacbed06cce3566216
SangJoon Lee <qkqh34k@gmail.com>
2023-04-25 15:37:45 +0900

    BaseEntity 기본생성자 추가

--- src/main/java/com/ssang/gtd/entity/BaseEntity.java
+++ src/main/java/com/ssang/gtd/entity/BaseEntity.java
@@ -2,7 +2,9 @@ package com.ssang.gtd.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
     @CreatedDate
     private LocalDateTime createDate;
